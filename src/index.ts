bb46ec460596acc5a691294beb6b314cde402cab
Yasir Khan <yasirkhan54@gmail.com>
2023-04-07 04:00:38 +0500

    package.json changed,

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
 import createHttpError, { HttpError } from 'http-errors';
-import { graphqlHTTP } from 'express-graphql';
 
 import { ERROR_MESSAGE } from './shared';
-import { ROOT_QUERY_LIST } from './root_queries';
+import { MODULES_LIST } from './modules';
 
 const app: Application = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-ROOT_QUERY_LIST.map((rootQuery) => { app.use(rootQuery.path, graphqlHTTP({ schema: rootQuery.schema, graphiql: rootQuery.graphiql })) })
+MODULES_LIST.forEach((module) => app.use(module.PATH, module.ROUTER))
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
