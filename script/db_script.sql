5a81329a9929571e4a48ad65f11cc1023c7e9061
Yasir Khan <yasirkhan54@gmail.com>
2023-04-20 03:25:38 +0500

    package removed and db script updated.

--- script/db_script.sql
+++ script/db_script.sql
@@ -1,110 +1,111 @@
-CREATE TABLE leads (
+CREATE TABLE IF NOT EXISTS leads (
   lead_id SERIAL PRIMARY KEY,
-  external_lead_id TEXT,
-  traffic_source TEXT,
-  lead_cost NUMERIC,
-  total_revenue NUMERIC,
-  is_test_lead BOOLEAN
+  external_lead_id text,
+  traffic_source text,
+  lead_cost money,
+  total_revenue money,
+  is_test_lead boolean
 );
 
-CREATE TABLE lead_status (
+CREATE TABLE IF NOT EXISTS lead_status (
   lead_status_id SERIAL PRIMARY KEY,
   lead_id INTEGER REFERENCES leads (lead_id),
-  api_response_json JSON,
-  original_status TEXT,
-  current_status TEXT,
-  status_update_timestamp TIMESTAMP,
-  reject_reason TEXT
+  api_response_json json,
+  original_status text,
+  current_status text,
+  status_update_timestamp time,
+  reject_reason text
 );
 
-CREATE TABLE sessions (
+CREATE TABLE IF NOT EXISTS sessions (
   session_id SERIAL PRIMARY KEY,
   lead_id INTEGER REFERENCES leads (lead_id),
-  ip TEXT,
-  web_url TEXT,
-  traffic_brand_name TEXT,
-  traffic_source_detail TEXT,
-  traffic_source_type TEXT,
-  web_access_key TEXT,
-  web_session_id TEXT,
-  landing_page TEXT,
-  device_type TEXT
+  ip text,
+  web_url text,
+  traffic_brand_name text,
+  traffic_source_detail text,
+  traffic_source_type text,
+  web_access_key text,
+  web_session_id text,
+  landing_page text,
+  device_type text
 );
 
-CREATE TABLE consents (
+CREATE TABLE IF NOT EXISTS consents (
   consent_id SERIAL PRIMARY KEY,
-  lead_id INTEGER REFERENCES leads (lead_id),
-  tcpa_timestamp_traffic TEXT,
-  tcpa_timestamp_marketing TEXT,
-  tcpa_text_traffic TEXT,
-  tpca_timestamp_client TEXT
+  lead_id integer REFERENCES leads (lead_id),
+  tcpa_timestamp_traffic text,
+  tcpa_timestamp_marketing text,
+  tcpa_text_traffic text,
+  tcpa_timestamp_client text
 );
 
-CREATE TABLE third_party_tokens (
+CREATE TABLE IF NOT EXISTS third_party_tokens (
   third_party_tokens_id SERIAL PRIMARY KEY,
-  lead_id INTEGER REFERENCES leads (lead_id),
-  traffic_jornaya_lead_id TEXT,
-  traffic_trusted_form_url TEXT,
-  traffic_trustedform_token TEXT
+  lead_id integer REFERENCES leads (lead_id),
+  traffic_jornaya_lead_id text,
+  traffic_trusted_form_url text,
+  traffic_trustedform_token text
 );
 
-CREATE TABLE utms (
+CREATE TABLE IF NOT EXISTS utms (
   utm_id SERIAL PRIMARY KEY,
-  lead_id INTEGER REFERENCES leads (lead_id),
-  source TEXT,
-  medium TEXT,
-  campaign TEXT,
-  content TEXT,
-  term TEXT,
-  supplier_id TEXT,
-  sub_id TEXT,
-  ad_id TEXT
+  lead_id integer REFERENCES leads (lead_id),
+  source text,
+  medium text,
+  campaign text,
+  content text,
+  term text,
+  supplier_id text,
+  sub_id text,
+  ad_id text
 );
 
-CREATE TABLE profiles (
+CREATE TABLE IF NOT EXISTS profiles (
   profile_id SERIAL PRIMARY KEY,
-  lead_id INTEGER REFERENCES leads (lead_id),
-  salutation TEXT,
-  first_name TEXT,
-  last_name TEXT,
-  phone_primary TEXT,
-  phone_secondary TEXT,
-  email TEXT
+  lead_id integer REFERENCES leads (lead_id),
+  salutation text,
+  first_name text,
+  last_name text,
+  phone_primary text,
+  phone_secondary text,
+  email text
 );
 
-CREATE TABLE qualifications (
+CREATE TABLE IF NOT EXISTS addresses (
+  address_id SERIAL PRIMARY KEY,
+  profile_id integer REFERENCES profiles (profile_id),
+  address_line_one text,
+  address_line_two text,
+  city text,
+  state text,
+  zip_code text
+);
+
+CREATE TABLE IF NOT EXISTS qualifications (
   qualification_id SERIAL PRIMARY KEY,
-  profile_id INTEGER REFERENCES profiles (profile_id),
-  lead_id INTEGER REFERENCES leads (lead_id),
-  enrollment_timeline JSON,
-  current_edu_level TEXT,
-  preferred_edu_level TEXT,
-  learning_preference TEXT,
+  profile_id integer REFERENCES profiles (profile_id),
+  lead_id integer REFERENCES leads (lead_id),
+  enrollment_timeline json,
+  current_edu_level text,
+  preferred_edu_level text,
+  learning_preference text,
   is_graduated_in_us BOOLEAN,
-  computer_internet_access TEXT,
-  level_of_interest TEXT,
-  time_of_day TEXT,
+  computer_internet_access text,
+  level_of_interest text,
+  time_of_day text,
   previously_contacted BOOLEAN
 );
 
-CREATE TABLE addresses (
-  address_id SERIAL PRIMARY KEY,
-  profile_id INTEGER REFERENCES profiles (profile_id),
-  address_line_one TEXT,
-  address_line_two TEXT,
-  city TEXT,
-  state TEXT,
-  zip_code TEXT
-);
-
-CREATE TABLE profile_criteria (
+CREATE TABLE IF NOT EXISTS profile_criteria (
   profile_criteria_id SERIAL PRIMARY KEY,
-  lead_id INTEGER REFERENCES leads (lead_id),
-  age INTEGER,
-  gender TEXT,
-  is_us_citizen BOOLEAN,
-  is_us_military BOOLEAN,
-  military_status TEXT,
-  military_branch TEXT,
-  high_school_graduation_year INTEGER
+  profile_id integer REFERENCES profiles (profile_id),
+  lead_id integer REFERENCES leads (lead_id),
+  age integer,
+  gender text,
+  is_us_citizen boolean,
+  is_us_military boolean,
+  military_status boolean,
+  military_branch text,
+  high_school_graduation_year integer
 );
