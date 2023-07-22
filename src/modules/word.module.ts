f988113b1ea8f198b993d876538c03145a407c53
Yasir Khan <yasir.buland@data-pilot.com>
2023-07-23 00:02:07 +0500

    updated code

--- src/modules/word.module.ts
+++ src/modules/word.module.ts
@@ -1,48 +1,54 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import createError from 'http-errors'
-import { ERROR_MESSAGE } from '../shared'
-import puppeteer from 'puppeteer-core'
+import fs from 'fs';
+import Docxtemplater from 'docxtemplater';
+import htmlToText from 'html-to-text';
 
 const router: Router = Router()
 
-// Function to generate PDF from HTML using Puppeteer
-async function generatePdfFromHtml(htmlContent) {
-	console.log('generating pdf from html');
-
-	const browser = await puppeteer.launch({
-		executablePath: process.env.GOOGLE_CHROME_BIN,
-		args: ['--no-sandbox', '--disable-setuid-sandbox'],
-		headless: true
-	});
-	console.log('browser launched');
-
-	const page = await browser.newPage();
-	console.log('page created');
-
-	await page.setContent(htmlContent);
-	console.log('page content set');
-
-	const pdfBuffer = await page.pdf();
-	console.log('pdf generated');
-
-	await browser.close();
-	console.log('browser closed');
-
-	return pdfBuffer;
-}
-
-router.post('/html-to-pdf', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/html-to-word', async (req: Request, res: Response, next: NextFunction) => {
 	const { html } = req.body;
 
+	if (!html) {
+		return res.status(400).json({ error: 'HTML content not provided in the request body.' });
+	}
+
 	try {
-		const pdfBuffer = await generatePdfFromHtml(html);
-		res.setHeader('Content-Type', 'application/pdf');
-		res.setHeader('Content-Disposition', 'attachment; filename=generated_pdf.pdf');
-		res.send(pdfBuffer);
-	} catch (err) {
-		res.status(500).json({ error: `PDF generation failed: ${err.message}` });
+		// Convert HTML to plain text
+		const contentText = htmlToText.fromString(html, {
+			wordwrap: 130 // Adjust the word wrap as needed
+		});
+
+		// Load the Word template
+		const doc: any = new Docxtemplater();
+		doc.loadFromString(contentText);
+
+		// Data to replace placeholders in the HTML template
+		// In this example, we use hardcoded data, but you can customize it based on your requirements
+		const data = {
+			title: 'My HTML to Word Conversion',
+			content: html
+		};
+
+		// Set the data to fill the placeholders in the template
+		doc.setData(data);
+
+		// Render the document
+		doc.render();
+
+		// Get the output as a Buffer
+		const buffer = doc.getZip().generate({ type: 'nodebuffer' });
+
+		// Set response headers for the Word document download
+		res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+		res.setHeader('Content-Disposition', 'attachment; filename="generated_word.docx"');
+
+		// Send the generated Word document in the response
+		res.send(buffer);
+	} catch (error) {
+		console.error('Error generating Word document:', error);
+		res.status(500).json({ error: 'An error occurred while generating the Word document.' });
 	}
 });
 
-export const PDF_PATH = '/pdf';
+export const PDF_PATH = '/word';
 export const PDF_ROUTER = router;
